f34fae3e8cc4f525ec0bd95fc8dc388824495a45
Mark Cantrill <mark@astro-designs.com>
2020-05-27 06:21:58 +0100

    Working with the Pi HQ camera

--- python/capture.py
+++ python/capture.py
@@ -55,7 +55,7 @@ GPIO.output(StatusLED, False)
 # Setup camera...
 camera = PiCamera()
 camera.rotation = 0
-camera.resolution = (1920, 1080)
+#camera.resolution = (640,480)
 camera.framerate = 15
 camera.start_preview(alpha = 200)
 camera.annotate_text_size = 50
@@ -73,8 +73,8 @@ def Capture(PreviewWait = 0, Delay = 0):
 	TimeNow = time.time()
 	NextCaptureTime = TimeNow + Delay
 
-	print("Starting preview...")
-	camera.start_preview()
+	#print("Starting preview...")
+	#camera.start_preview()
 	
 	for i in range(NumFrames):
 		#print("Starting preview...")
@@ -99,6 +99,14 @@ def Capture(PreviewWait = 0, Delay = 0):
 		Filepath = "../media/" + Filename + "_" + TimeStr + ".jpg"
 		print(Filepath)
 		
+		# Start preview...
+		print("Starting preview...")
+		camera.start_preview()
+		
+		# Camera warm-up time
+		print("Warming up...")
+		time.sleep(2)
+		
 		# Hold preview until trigger released if in PreviewTrigger mode...
 		if PreviewWait == 1: # Wait for PreviewTrigger to be released
 			while GPIO.input(PreviewTrigger) == False:
@@ -121,14 +129,14 @@ def Capture(PreviewWait = 0, Delay = 0):
 			time.sleep(VideoDuration)
 			#camera.stop_recording()
 
-		#print("Stopping preview...")
-		#camera.stop_preview()	
+		print("Stopping preview...")
+		camera.stop_preview()	
 		GPIO.output(StatusLED, False)
 		NextCaptureTime = NextCaptureTime + FrameInterval
 		time.sleep(3)
 
-	print("Stopping preview...")
-	camera.stop_preview()
+	#print("Stopping preview...")
+	#camera.stop_preview()
 
 def TriggerMonitor():
 	global TimeNow, NextCaptureTime, Finished
