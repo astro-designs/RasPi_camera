aa1a0783aa21b07b7f9d48d28fbd4f4cd98c4d39
Mark Cantrill <mark@astro-designs.com>
2020-05-30 00:44:35 +0100

    Changing configuration file name Adding ftp changing resolution format

---
+++ python/camera_conf.py
@@ -0,0 +1,46 @@
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi WebCam</title>
+</head>
+<body>
+<center><h1>Raspberry Pi WebCam</h1></center>
+<center><img src="stream.mjpg" width="1920" height="1080"></center>
+</body>
+</html>
+"""
+
+# Supported resolutions:
+# Be sure to set the resolution in the html above to the same resolution
+#resolution=(640,480)
+#resolution=(800,600)
+#resolution=(1024,768)
+#resolution=(1920,1080)
+#resolution=(1296,972)
+resolution=(2592,1944) # 5MP sensor resolution
+#resolution=(3280,2464) # 8MP sensor resolution
+#resolution=(4056,3040) # 12.3MP sensor resolution
+
+framerate=1
+#framerate=24
+#framerate=48
+#framerate=25
+#framerate=50
+#framerate=29.97
+#framerate=59.94
+#framerate=30
+#framerate=60
+
+#rotation=0
+rotation = 90
+#rotation = 180
+#rotation = 270
+
+# Webcam settings...
+webcam_port = 8005
+
+# FTP settings...
+ftp_server = 'server'
+ftp_user = 'username'
+ftp_password = 'password'
+ftp_path = 'folder'
